0c1b6680c74f0583f7288a4a282214af70f83894
Caner Karaoglu <38299262+theVirus94@users.noreply.github.com>
2022-07-26 00:21:47 +0300

    Quit bitching about fancy GPU learning, moved CPU learning(same speed somehow lol)

--- Final Project/training.py
+++ Final Project/training.py
@@ -21,18 +21,17 @@ def simple_train(model_args, batch_size, max_epochs, dataset):
 
   # Train model
   model = TordeuxNet(model_args)
-  trainer = pl.Trainer(accelerator="gpu", devices=1, max_epochs = max_epochs, callbacks=[EarlyStopping(monitor="val_loss", mode="min", patience=10)])
+  trainer = pl.Trainer(max_epochs = max_epochs, callbacks=[EarlyStopping(monitor="val_loss", mode="min", patience=10)])
   trainer.fit(model, train_loader, val_loader)
 
   # Compute test MSE
   test_mse = trainer.predict(model, test_loader)
 
   # Prints, plots and returns
-  """model = model.to("cpu")
   plt.plot(range(len(model.training_metrics)), model.training_metrics, label = "Training loss")
   plt.plot(range(len(model.validation_metrics)), model.validation_metrics, label = "Validation loss")
   plt.legend()
-  plt.show()"""
+  plt.show()
 
   print("Last epoch training loss: {}".format(model.training_metrics[-1]))
   print("Last epoch validation loss: {}".format(model.validation_metrics[-1]))
